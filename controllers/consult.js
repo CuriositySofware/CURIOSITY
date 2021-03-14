4f0f7040786b92e0338d7336bca75f083ba33dfe
Marcitox <benitez4721@gmail.com>
2021-03-14 12:54:00 -0400

    fix

--- controllers/consult.js
+++ controllers/consult.js
@@ -8,7 +8,7 @@ const prefixs = `
       PREFIX cit: <http://curiocity.org/>
       `;
 const consult = (req, res = response) => {
-  const { author="arte", material, place, title } = req.body;
+  const { author = "arte", material, place, title } = req.body;
 
   // Query por el momento cableada
   const query = `${prefixs} SELECT ?labelArtifact ?labelMaterial ?labelKeeper ?labelCreator ?id
@@ -25,40 +25,39 @@ const consult = (req, res = response) => {
     ?keeper rdfs:label ?labelKeeper .
     ?creator rdfs:label ?labelCreator .
     ${
-      author ?
-      `FILTER( regex(lcase(?labelCreator), "${author.toLowerCase()}" )) .` : ""
+      author
+        ? `FILTER( regex(lcase(?labelCreator), "${author.toLowerCase()}" )) .`
+        : ""
     }       
     ${
-      material ?
-      `FILTER( regex(lcase(?labelMaterial), "${material.toLowerCase()}" )) .` : ""
+      material
+        ? `FILTER( regex(lcase(?labelMaterial), "${material.toLowerCase()}" )) .`
+        : ""
     }
     ${
-      title ?
-      `FILTER( regex(lcase(?labelArtifact), "${title.toLowerCase()}" )) .` : ""
+      title
+        ? `FILTER( regex(lcase(?labelArtifact), "${title.toLowerCase()}" )) .`
+        : ""
     }
     ${
-      place ?
-      `FILTER( regex(lcase(?labelKeeper), "${place.toLowerCase()}" )) .` : ""
+      place
+        ? `FILTER( regex(lcase(?labelKeeper), "${place.toLowerCase()}" )) .`
+        : ""
     }
     
   }`;
 
-
-  // Aca se envia la consulta a graphDB(por ahora en local)
-  fetch(
-    `http://localhost:3030/CURIOSITY/sparql`,
-    {
-      method: "POST",
-      headers: {
-        "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
-        accept: "application/sparql-results+json",
-      },
-      body: new URLSearchParams({
-        query,
-      }),
-      
-    }
-  )
+  // Aca se envia la consulta a Jena(por ahora en local)
+  fetch(`http://localhost:3030/CURIOSITY/sparql`, {
+    method: "POST",
+    headers: {
+      "content-type": "application/x-www-form-urlencoded;charset=UTF-8",
+      accept: "application/sparql-results+json",
+    },
+    body: new URLSearchParams({
+      query,
+    }),
+  })
     .then((resp) => resp.json())
     .then((resp) => {
       const result = resp.results.bindings;
