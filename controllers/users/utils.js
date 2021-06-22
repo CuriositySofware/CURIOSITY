de8b39106c137433e8025d890f187a2d0059b88f
Carlos Gonzalez <cgonzalez@teravisiontech.com>
2021-06-22 14:58:16 -0400

    Agregado email en userInfo

--- controllers/users/utils.js
+++ controllers/users/utils.js
@@ -74,10 +74,12 @@ const userData = async (email) => {
     .then((resp) => {
       const result = resp.results.bindings;
       if (result && result.length > 0) {
+        console.log(result);
         return {
           ok: true,
           first_name: result[0].first_name.value,
           last_name: result[0].last_name.value,
+          email: result[0].email.value,
           type: result[0].user_type.value.includes("visitor")
             ? "visitor"
             : "admin",
